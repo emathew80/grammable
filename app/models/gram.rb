4b56afcb1dca2d811bcb932032084b1aaee5ba9d
Elson Mathew <Mathew@Elsons-MacBook-Pro.local>
2017-01-10 11:44:36 -0500

    Add style to app

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -1,6 +1,7 @@
 class Gram < ActiveRecord::Base
   mount_uploader :picture, PictureUploader
   belongs_to :user
+  has_many :comments
   validates :message, presence: true
   validates :picture, presence: true
 
